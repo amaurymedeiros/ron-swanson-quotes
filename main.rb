3bff00fdad9ffcb77124b1e7dedd942bdecbcec9
Amaury Medeiros <amaurymedeiros@gmail.com>
2015-03-21 16:28:21 -0300

    rack up

--- main.rb
+++ main.rb
@@ -3,8 +3,10 @@
 require 'sinatra'
 require 'slim'
 
-get '/' do
-  require_relative 'ron_swanson_quote.rb'
-  @quote = RonSwansonQuote.generate_quote
-  slim :index
+class SwansonApp < Sinatra::Base
+  get '/' do
+    require_relative 'ron_swanson_quote.rb'
+    @quote = RonSwansonQuote.generate_quote
+    slim :index
+  end
 end
