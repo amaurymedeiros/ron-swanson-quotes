5b9c68bdd31bcb1feb5b0674731d59fd8c0c0618
Raissa Sarmento <raissaa@gmail.com>
2015-03-24 11:39:05 -0300

    Change the way of creating the strings

--- ron_swanson_quote.rb
+++ ron_swanson_quote.rb
@@ -2,187 +2,187 @@ class RonSwansonQuote
 
   def self.quotes
     [
-      'When people get a little too chummy with me I like to call them by the wrong name to let them know I don\'t really care about them.',
+      %{When people get a little too chummy with me I like to call them by the wrong name to let them know I don't really care about them.},
 
-      'When I eat, it is the food that is scared.',
+      %{When I eat, it is the food that is scared.},
 
-      'Honour: If you need it defined, you don\'t have it.',
+      %{Honour: If you need it defined, you don't have it.},
 
-      'Capitalism: God\'s way of determining who is smart and who is poor.',
+      %{Capitalism: God's way of determining who is smart and who is poor.},
 
-      'Never been hungover. After I\'ve had too much whiskey, I cook myself a large flank steak, pan-fried in salted butter.
-      I eat that, put on a pair of wet socks and go to sleep.',
+      %{Never been hungover. After I've had too much whiskey, I cook myself a large flank steak, pan-fried in salted butter.
+      I eat that, put on a pair of wet socks and go to sleep.},
 
-      'There is no wrong way to consume alcohol.',
+      %{There is no wrong way to consume alcohol.},
 
-      'Fishing relaxes me. It\'s like yoga, except I still get to kill something.',
+      %{Fishing relaxes me. It's like yoga, except I still get to kill something.},
 
-      'Just give me all the bacon and eggs you have. Wait... wait. I worry that what you just heard was:
-      Give me a lot of bacon and eggs. What I said was: Give me ALL the bacon and eggs you have. Do you understand?',
+      %{Just give me all the bacon and eggs you have. Wait... wait. I worry that what you just heard was:
+      Give me a lot of bacon and eggs. What I said was: Give me ALL the bacon and eggs you have. Do you understand?},
 
-      'There\'s only one thing I hate more than lying. Skim milk. Which is water that is lying about being milk.',
+      %{There's only one thing I hate more than lying. Skim milk. Which is water that is lying about being milk.},
 
-      'Never half-ass two things. Whole-ass one thing.',
+      %{Never half-ass two things. Whole-ass one thing.},
 
-      'The less I know about other people\'s affairs, the happier I am. I\'m not interested about caring about people.
-      I once worked with a guy for three years and never learned his name. Best friend I ever had. We still never talk sometimes.',
+      %{The less I know about other people's affairs, the happier I am. I'm not interested about caring about people.
+      I once worked with a guy for three years and never learned his name. Best friend I ever had. We still never talk sometimes.},
 
-      'Any dog under fifty pounds is a cat and cats are useless.',
+      %{Any dog under fifty pounds is a cat and cats are useless.},
 
-      'Crying: acceptable at funerals and the Grand Canyon.',
+      %{Crying: acceptable at funerals and the Grand Canyon.},
 
-      'Under my tutelage, you will grow from boys to men. From men into gladiators. And from gladiators into Swansons.',
+      %{Under my tutelage, you will grow from boys to men. From men into gladiators. And from gladiators into Swansons.},
 
-      '[On bowling] Straight down the middle. No hook, no spin, no fuss. Anything more and this becomes figure skating.',
+      %{[On bowling] Straight down the middle. No hook, no spin, no fuss. Anything more and this becomes figure skating.},
 
-      'The government is a greedy piglet that suckles on a taxpayer\'s teat until they have sore, chapped nipples.',
+      %{The government is a greedy piglet that suckles on a taxpayer's teat until they have sore, chapped nipples.},
 
-      'It\'s always a good idea to demonstrate to your coworkers that you are capable of withstanding a tremendous amount of pain.',
+      %{It's always a good idea to demonstrate to your coworkers that you are capable of withstanding a tremendous amount of pain.},
 
-      'Fish, for sport only, not for meat. Fish meat is practically a vegetable.',
+      %{Fish, for sport only, not for meat. Fish meat is practically a vegetable.},
 
-      'There is only one bad word: taxes.',
+      %{There is only one bad word: taxes.},
 
-      'History began July 4th, 1776. Anything before that was a mistake.',
+      %{History began July 4th, 1776. Anything before that was a mistake.},
 
-      'Give a man a fish and feed him for a day. Don\'t teach a man to fish... and feed yourself. He\'s a grown man. And fishing\'s not that hard.',
+      %{Give a man a fish and feed him for a day. Don't teach a man to fish... and feed yourself. He's a grown man. And fishing's not that hard.},
 
-      'Great job, everyone. The reception will be held in each of our individual houses, alone.',
+      %{Great job, everyone. The reception will be held in each of our individual houses, alone.},
 
-      'Friends: one to three is sufficient.',
+      %{Friends: one to three is sufficient.},
 
-      'I like saying "No", it lowers their enthusiasm.',
+      %{I like saying "No", it lowers their enthusiasm.},
 
-      'You had me at "meat tornado".',
+      %{You had me at "meat tornado".},
 
-      'Clear alcohols are for rich women on diets.',
+      %{Clear alcohols are for rich women on diets.},
 
-      'I call this turf \'n\' turf. It\'s a 16 oz T-bone and a 24 oz porterhouse. Also, whiskey and a cigar. I am going to consume all of this at the same time because I am a free American.',
+      %{I call this turf 'n' turf. It's a 16 oz T-bone and a 24 oz porterhouse. Also, whiskey and a cigar. I am going to consume all of this at the same time because I am a free American.},
 
-      'There will be alcohol there, so I will go as well.',
+      %{There will be alcohol there, so I will go as well.},
 
-      'Intensity: Give 100%. 110% is impossible. Only idiots recommend that.',
+      %{Intensity: Give 100%. 110% is impossible. Only idiots recommend that.},
 
-      'An ideal night out is stepping onto my porch area and grilling up a thick slab of something\'s flesh.',
+      %{An ideal night out is stepping onto my porch area and grilling up a thick slab of something's flesh.},
 
-      'On my deathbed, my final wish is to have my ex-wives rush to my side so I can use my dying breath to tell them both to go to hell one last time.',
+      %{On my deathbed, my final wish is to have my ex-wives rush to my side so I can use my dying breath to tell them both to go to hell one last time.},
 
-      'I don\'t like loud noises and people making a fuss. And I especially don\'t like people celebrating, because they know a piece of private information about me. Plus the whole thing is a scam. Birthdays were invented by Hallmark to sell cards.',
+      %{I don't like loud noises and people making a fuss. And I especially don't like people celebrating, because they know a piece of private information about me. Plus the whole thing is a scam. Birthdays were invented by Hallmark to sell cards.},
 
-      'I also think it\'s pointless for a human to paint scenes of nature when they can go outside and stand in it.',
+      %{I also think it's pointless for a human to paint scenes of nature when they can go outside and stand in it.},
 
-      'I\'m starving. I only had one breakfast.',
+      %{I'm starving. I only had one breakfast.},
 
-      'Creativity is for people with glasses who like to lie.',
+      %{Creativity is for people with glasses who like to lie.},
 
-      'If any of you need anything at all... Too bad.',
+      %{If any of you need anything at all... Too bad.},
 
-      'I\'m not sure I\'m interested in that. No, I am sure. I am not interested in that.',
+      %{I'm not sure I'm interested in that. No, I am sure. I am not interested in that.},
 
-      'I\'m surrounded by a lot of women in this department. And that includes the men.',
+      %{I'm surrounded by a lot of women in this department. And that includes the men.},
 
-      'I like Tom. He doesn\'t do a lot of work around here.',
+      %{I like Tom. He doesn't do a lot of work around here.},
 
-      'I was born ready. I\'m Ron F*cking Swanson.',
+      %{I was born ready. I'm Ron F*cking Swanson.},
 
-      'The whole point of this country is if you want to eat garbage, balloon up to
-      600 pounds and die of a heart attack at 43, you can! You are free to do so. To me, that\'s beautiful.',
+      %{The whole point of this country is if you want to eat garbage, balloon up to
+      600 pounds and die of a heart attack at 43, you can! You are free to do so. To me, that's beautiful.},
 
-      'Why is everyone else so bad at eating?',
+      %{Why is everyone else so bad at eating?},
 
-      'Keep your tears in your eyes where they belong.',
+      %{Keep your tears in your eyes where they belong.},
 
-      'I would sooner visit Europe than have something romantic happen between us.',
+      %{I would sooner visit Europe than have something romantic happen between us.},
 
-      'I enjoy government functions like I enjoy getting kicked in the nuggets with a steel toed boot.
-      But this hotel always served bacon wrapped shrimp. That\'s my number one favorite food wrapped around my number three favorite food.
-      I\'d go to a banquet in honor of those Somali pirates if they served bacon wrapped shrimp.',
+      %{I enjoy government functions like I enjoy getting kicked in the nuggets with a steel toed boot.
+      But this hotel always served bacon wrapped shrimp. That's my number one favorite food wrapped around my number three favorite food.
+      I'd go to a banquet in honor of those Somali pirates if they served bacon wrapped shrimp.},
 
-      'Yep. My ex-wife Tammy cheated on me, then we divorced, then last week I ran into her sister
-      Beth here, turns out she hates Tammy too, so we started dating. It\'s like a fairytale.',
+      %{Yep. My ex-wife Tammy cheated on me, then we divorced, then last week I ran into her sister
+      Beth here, turns out she hates Tammy too, so we started dating. It's like a fairytale.},
 
-      'There is no need for medical treatment. I\'m on a government health plan. I don\'t need tax payers
-      to pay for me to lay in bed and eat jello and watch Dr. Phil.',
+      %{There is no need for medical treatment. I'm on a government health plan. I don't need tax payers
+      to pay for me to lay in bed and eat jello and watch Dr. Phil.},
 
-      'I have a hernia. Had it for awhile, and I\'ve been ignoring it... successfully. But, ah, this
-      morning I made the mistake of sneezing. But as long as I sit still, don\'t move my head or torso, I\'m good. I got this.',
+      %{I have a hernia. Had it for awhile, and I've been ignoring it... successfully. But, ah, this
+      morning I made the mistake of sneezing. But as long as I sit still, don't move my head or torso, I'm good. I got this.},
 
-      'To exes. May they always stay that way.',
+      %{To exes. May they always stay that way.},
 
-      '[on his ex-wife] Every time she laughs an Angel dies. Even telemarketers avoid her. Her birth was payback for the sins of man.',
+      %{[on his ex-wife] Every time she laughs an Angel dies. Even telemarketers avoid her. Her birth was payback for the sins of man.},
 
-      'I generally like my job, because for most of the year, there isn\'t a lot to do.
-      And around Christmas most of the people go away, so there\'s even less to do. So, yes, I love Christmas.',
+      %{I generally like my job, because for most of the year, there isn't a lot to do.
+      And around Christmas most of the people go away, so there's even less to do. So, yes, I love Christmas.},
 
-      'My house has no walkway, no porch light, no welcome mat and no visible front door.
+      %{My house has no walkway, no porch light, no welcome mat and no visible front door.
       It is virtually impossible to get into my house. I keep the address unlisted, I change the house number every six
-      weeks and the mailman\'s instructed to leave the mail in a small plastic box hanging from my fence by a metal chain.
-      Most of my house is a wood shop and the rest is a space for meat curing.',
+      weeks and the mailman's instructed to leave the mail in a small plastic box hanging from my fence by a metal chain.
+      Most of my house is a wood shop and the rest is a space for meat curing.},
 
-      'I couldn\'t be happier about this. I think the entire government should be privatized. Chuck E. Cheese
-      could run the parks. Everything operated by tokens. Drop in a token go on the swing set. Drop in another token, take a walk. Drop in a token look at a duck.',
+      %{I couldn't be happier about this. I think the entire government should be privatized. Chuck E. Cheese
+      could run the parks. Everything operated by tokens. Drop in a token go on the swing set. Drop in another token, take a walk. Drop in a token look at a duck.},
 
-      'Leslie, my first wife Tammy, tried throwing me a surprise birthday party. When I saw my friends hiding through the window,
-      I drove to a gas station, called the cops and told \'em people had broken into my home. I\'m not big on surprises.',
+      %{Leslie, my first wife Tammy, tried throwing me a surprise birthday party. When I saw my friends hiding through the window,
+      I drove to a gas station, called the cops and told 'em people had broken into my home. I'm not big on surprises.},
 
-      'The three most important people in a man\'s life are his barber, his butcher, and his lover.',
+      %{The three most important people in a man's life are his barber, his butcher, and his lover.},
 
-      'There will be no fouffy deserts. There will be no giant soaked bubble guns.
-      There will be no adult men in costumes. And most of all there will be no f*cking vegetables.',
+      %{There will be no fouffy deserts. There will be no giant soaked bubble guns.
+      There will be no adult men in costumes. And most of all there will be no f*cking vegetables.},
 
-      'Every summer Leslie throws a barbecue to thank the parks and maintenance staff. It\'s horrifying.
-      Barbecues should be about one thing: good shared meat.',
+      %{Every summer Leslie throws a barbecue to thank the parks and maintenance staff. It's horrifying.
+      Barbecues should be about one thing: good shared meat.},
 
-      'Ah, Councilwoman. In honor of your never-ending quest to personally babysit each and every American citizen,
+      %{Ah, Councilwoman. In honor of your never-ending quest to personally babysit each and every American citizen,
       I went to Paunch Burger and got myself a Number Two: Double Bacon Grenade Deluxe, Hash Browns, Chili Cheese Fries,
-      and one poached egg. [...] I also picked up a 64 oz Sweetums Sugar Splash. Dammit, I love this country so much.',
+      and one poached egg. [...] I also picked up a 64 oz Sweetums Sugar Splash. Dammit, I love this country so much.},
 
-      'I will not report it to the judge, but if you slip up again you will have much more to fear than some feeble government employee in a robe.',
+      %{I will not report it to the judge, but if you slip up again you will have much more to fear than some feeble government employee in a robe.},
 
-      'When you do get your phone back you will not stare at it when you are talking to another human being. Look a man in the eye when you speak with him.',
+      %{When you do get your phone back you will not stare at it when you are talking to another human being. Look a man in the eye when you speak with him.},
 
-      'I\'m proud of you, but also fearful that we’re verging on what I call feelings territory. So let\'s stare at the fire in silence.',
+      %{I'm proud of you, but also fearful that we’re verging on what I call feelings territory. So let's stare at the fire in silence.},
 
-      'I am not sure if I’m ready for a whole family. If the kids ever wanted to come to my place, I’d have to take a whole week off work just to undo the alarms and tripwires.',
+      %{I am not sure if I’m ready for a whole family. If the kids ever wanted to come to my place, I’d have to take a whole week off work just to undo the alarms and tripwires.},
 
-      'I\'m always looking for new investments. Ever since I got my first job at the age of nine,
-      I\'ve put all my money in gold, which is currently at an all time high. So I have a certain amount of money. I\'ve said too much.',
+      %{I'm always looking for new investments. Ever since I got my first job at the age of nine,
+      I've put all my money in gold, which is currently at an all time high. So I have a certain amount of money. I've said too much.},
 
-      'Haircuts: There are three acceptable haircuts. High and tight, crew cut, buzz cut.',
+      %{Haircuts: There are three acceptable haircuts. High and tight, crew cut, buzz cut.},
 
-      'Libertarianism is all about individual liberty, and it should never be defined by the terms liberal or conservative.',
+      %{Libertarianism is all about individual liberty, and it should never be defined by the terms liberal or conservative.},
 
-      'If there were more food and fewer people, this would be a perfect party.',
+      %{If there were more food and fewer people, this would be a perfect party.},
 
-      'Women like scars. It shows you survived an attack, and they\'ll assume that attack was from a man.',
+      %{Women like scars. It shows you survived an attack, and they'll assume that attack was from a man.},
 
-      'That\'s ridiculous, I don\'t think of you romantically. You\'re pro government, you never stop talking, and you have blonde hair. You\'re my worst nightmare.',
+      %{That's ridiculous, I don't think of you romantically. You're pro government, you never stop talking, and you have blonde hair. You're my worst nightmare.},
 
-      'Buffets: Whenever available. Choose quantity over quality.',
+      %{Buffets: Whenever available. Choose quantity over quality.},
 
-      'America: The only Country that matters. If you want to experience other "cultures," use an atlas or a ham radio.',
+      %{America: The only Country that matters. If you want to experience other "cultures," use an atlas or a ham radio.},
 
-      'Greatness Itself: The best revenge.',
+      %{Greatness Itself: The best revenge.},
 
-      'Discipline: The ability to repeat a boring thing over & over again.',
+      %{Discipline: The ability to repeat a boring thing over & over again.},
 
-      'Stillness: Don\'t waste energy moving unless necessary.',
+      %{Stillness: Don't waste energy moving unless necessary.},
 
-      'Property Rights: They exist. Do not let them be taken away from you.',
+      %{Property Rights: They exist. Do not let them be taken away from you.},
 
-      'Perspiration: Only sweat during physical activity or love making. No emotional sweating.',
+      %{Perspiration: Only sweat during physical activity or love making. No emotional sweating.},
 
-      'Frankness: Cut the B.S.',
+      %{Frankness: Cut the B.S.},
 
-      'Rage: One rage every three months is permitted. Try not to hurt anyone who doesn\'t deserve it.',
+      %{Rage: One rage every three months is permitted. Try not to hurt anyone who doesn't deserve it.},
 
-      'Handshakes: Firm, dry, solid, 3 seconds.'
+      %{Handshakes: Firm, dry, solid, 3 seconds.}
     ]
 
   end
 
   def self.generate_quote
-    '"' + quotes.sample + '"'
+    %{"#{quotes.sample}"}
   end
 
 end
