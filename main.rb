40a4186862756842e1a5838a8e24bdd706c064e3
Raissa Sarmento <raissaa@gmail.com>
2015-12-30 15:37:02 -0300

    Get another quote via ajax

--- main.rb
+++ main.rb
@@ -2,11 +2,15 @@
 
 require 'sinatra'
 require 'slim'
+require_relative 'ron_swanson_quote.rb'
 
 class SwansonApp < Sinatra::Base
   get '/' do
-    require_relative 'ron_swanson_quote.rb'
     @quote = RonSwansonQuote.generate_quote
     slim :index
+  end
+
+  get '/another-quote' do
+  	RonSwansonQuote.generate_quote
   end
 end
